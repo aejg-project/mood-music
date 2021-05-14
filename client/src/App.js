cc6d5f36aa2d42a3b4525b7e5623cc2a1a47a5f2
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-05-13 19:37:29 -0700

    import Google Fonts

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { Container, Navbar, Nav } from "react-bootstrap";
+import './index.css'
 
 import Header from './components/Header';
 import BookList from './pages/BookList';
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = ({ currentBook }) => {
     <header className="bg-info mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-          <h1>The Book Lounge</h1>
+          <h1>Mood Music</h1>
         </Link>
 
         <h3>Now Viewing: {currentBook.title || 'Your book list'}</h3>
